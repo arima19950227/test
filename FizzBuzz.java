2a5c55f9220fc353078d4eaacaf9e22542d13484
arima19950227 <arima.macbook@gmail.com>
2023-01-28 13:48:10 +0000

    奇数偶数

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,15 +1,17 @@
 class Main {
   public static void main(String[] args){
-    for(int i=1; i<=100; i++){
-      if(i%3==0 && i%5==0){
-        System.out.println("FizzBuzz");
-      }else if(i%3==0){
-        System.out.println("Fizz");
-      }else if(i%5==0){
-        System.out.println("Buzz");
-      }else{
-        System.out.println(i);
-      }
-    }
+   int[] numbers={1,4,6,9,13,16};
+   int oddSum=0;
+   int evenSum=0;
+   
+   for(int i:numbers){
+     if(i%2==0){
+       oddSum += i;
+     }else{
+       evenSum += i;
+     }
+   }
+   System.out.println("偶数の和は" + oddSum + "です");
+   System.out.println("奇数の和は"+ evenSum + "です");
   }
 }
